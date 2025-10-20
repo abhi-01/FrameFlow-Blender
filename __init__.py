9cdb95f19e71c3cc87258898d150c137995d87e9
abhi-01 <abhishek.physics90@gmail.com>
2025-10-20 20:33:35 +0530

    Just version renaming to v1.2.2

--- __init__.py
+++ __init__.py
@@ -11,7 +11,7 @@ import bpy
 bl_info = {
     "name": "Frame Flow Beta",
     "author": "Abhishek Kumar",
-    "version": (1, 2, 1),
+    "version": (1, 2, 2),
     "blender": (4, 3, 0),
     "location": "Node Editor > Sidebar > Node",
     "description": "Advanced Node Frame Add-on for Blender",
